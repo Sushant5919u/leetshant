b60117d284c1054e9458a21be30c971e05b1525c
Sushant Dipak Pande <111930830+Sushant5919u@users.noreply.github.com>
2026-06-25 17:57:59 +0530

    Runtime: 0 ms (100.00%), Memory: 44.3 MB (95.53%) - Gitcode

---
+++ 111-minimum-depth-of-binary-tree/111. Minimum Depth of Binary Tree.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from collections import deque
+class Solution:
+    def minDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        queue=deque([root])
+        ans=0
+        while queue:
+            ans+=1
+            level_size=len(queue)
+            for i in range(level_size):
+                node=queue.popleft()
+                if not node.left and not node.right:
+                    return ans
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+                
+               
+        
