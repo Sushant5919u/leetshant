e6d447cdb7f359851f97ce7c9620ec114858bb87
Sushant Dipak Pande <111930830+Sushant5919u@users.noreply.github.com>
2026-06-28 00:23:14 +0530

    Runtime: 0 ms (100.00%), Memory: 19.3 MB (41.85%) - Gitcode

--- 1480-running-sum-of-1d-array/1480. Running Sum of 1d Array.py
+++ 1480-running-sum-of-1d-array/1480. Running Sum of 1d Array.py
@@ -1,10 +1,8 @@
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
-        b=[]
-        for i in range(len(nums)):
-            a=0
-            for j in range(i+1):
-                a+=nums[j]
-            b.append(a)
+        b=[nums[0],]
+
+        for i in range(1,len(nums)):
+            b.append(b[i-1]+nums[i])
         return b
     
