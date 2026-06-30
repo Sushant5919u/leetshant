6ee178be48ddedc75f5d3fc252a01c16f452639d
Sushant Dipak Pande <111930830+Sushant5919u@users.noreply.github.com>
2026-06-30 15:22:20 +0530

    Runtime: 0 ms (100.00%), Memory: 19.3 MB (17.16%) - Gitcode

--- 1137-n-th-tribonacci-number/1137. N-th Tribonacci Number.py
+++ 1137-n-th-tribonacci-number/1137. N-th Tribonacci Number.py
@@ -4,11 +4,9 @@ class Solution:
             return n
         if n==2:
             return 1
-        dp=[-1]*(n+1)
 
-        dp[0],dp[1],dp[2]=0,1,1
+        prev1,prev2,prev3=0,1,1
         for i in range(3,n+1):
-            dp[i]=dp[i-1]+dp[i-2]+dp[i-3]
-        return dp[n]
-        
+            prev1,prev2,prev3=prev2,prev3,prev1+prev2+prev3
+        return prev3        
         
