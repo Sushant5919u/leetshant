ead719a15a671434369f28dce7d9e09edb3f3961
Sushant Dipak Pande <111930830+Sushant5919u@users.noreply.github.com>
2026-06-30 15:03:57 +0530

    Runtime: 32 ms (98.96%), Memory: 19.2 MB (56.42%) - Gitcode

--- 509-fibonacci-number/509. Fibonacci Number.py
+++ 509-fibonacci-number/509. Fibonacci Number.py
@@ -1,14 +1,11 @@
 class Solution:
     def fib(self, n: int) -> int:
-        dp = [-1] * (n + 1)
+        if n==1 or n==0:
+            return n
+        dp=[-1]*(n+1)
+
+        dp[0],dp[1]=0,1
+        for i in range(2,n+1):
+            dp[i]=dp[i-1]+dp[i-2]
+        return dp[n]
         
-        def memo(n: int) -> int:
-            if n == 0 or n == 1:
-                return n
-            if dp[n] != -1:
-                return dp[n]
-            
-            dp[n] = memo(n - 1) + memo(n - 2)
-            return dp[n]
-            
-        return memo(n)
