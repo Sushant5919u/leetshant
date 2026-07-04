7286ff7f783e5d57ec00e1172551092f401ebd83
Sushant Dipak Pande <111930830+Sushant5919u@users.noreply.github.com>
2026-07-05 01:30:23 +0530

    Runtime: 4 ms (70.52%), Memory: 20.1 MB (65.88%) - Gitcode

--- 45-jump-game-ii/45. Jump Game II.py
+++ 45-jump-game-ii/45. Jump Game II.py
@@ -4,22 +4,17 @@ class Solution:
         if b < 1:
             return 0
         
-        a = 0  # max_reachable
-        c = 0  # jumps counter
-        current_end = 0  # The end of the current jump's range
+        a = 0  
+        c = 0  
+        d = 0 
         
-        # We loop up to b (len(nums) - 1) because we don't need to jump if we are already at the last element
         for i in range(b):
             j = nums[i]
             a = max(a, i + j)
-            
-            # If we reached the furthest point possible for the current jump
-            if i == current_end:
-                c += 1               # We must make another jump
-                current_end = a      # Our new window extends to the maximum reachable point
-                
-                # If our new window can already touch or clear the end, we can stop early
-                if current_end >= b:
+            if i == d:
+                c += 1              
+                d = a 
+                if d >= b:
                     break
                     
         return c
