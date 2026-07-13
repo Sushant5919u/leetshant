ca12200107c796ecf4632bd6f6bc645982a849df
Sushant Dipak Pande <111930830+Sushant5919u@users.noreply.github.com>
2026-07-13 21:35:03 +0530

    Runtime: 35 ms (76.29%), Memory: 37.5 MB (60.60%) - Gitcode

---
+++ 1331-rank-transform-of-an-array/1331. Rank Transform of an Array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def arrayRankTransform(self, arr: List[int]) -> List[int]:
+
+        sorted_unique = sorted(list(set(arr)))
+  
+        rank_map = {num: rank + 1 for rank, num in enumerate(sorted_unique)}
+        
+        return [rank_map[num] for num in arr]
