e844400f2f69094203a5525d40808a6a3faff7eb
Sushant Dipak Pande <111930830+Sushant5919u@users.noreply.github.com>
2026-07-19 14:28:43 +0530

    Runtime: 0 ms (100.00%), Memory: 20.5 MB (40.77%) - Gitcode

--- 1-two-sum/1. Two Sum.py
+++ 1-two-sum/1. Two Sum.py
@@ -2,9 +2,9 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         a={}
         for i in range(len(nums)):
-            b=target - nums[i]
-            if b in a:
-                return a[b],i
+            if (target-nums[i]) in a:
+                return a[target-nums[i]],i
             else:
                 a[nums[i]]=i
+        
         
