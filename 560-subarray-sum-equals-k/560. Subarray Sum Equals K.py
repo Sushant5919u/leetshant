dc4f6f6121d9ba27154b2658a96bd23d9b1ec266
Sushant Dipak Pande <111930830+Sushant5919u@users.noreply.github.com>
2026-07-20 17:37:19 +0530

    Runtime: 31 ms (74.78%), Memory: 21.8 MB (55.86%) - Gitcode

--- 560-subarray-sum-equals-k/560. Subarray Sum Equals K.py
+++ 560-subarray-sum-equals-k/560. Subarray Sum Equals K.py
@@ -1,12 +1,15 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        res = 0
-        cur = 0
-        prefix = {0:1}
-        for i in nums:
-            cur += i
-            diff = cur - k
-            res += prefix.get(diff,0)
-            prefix[cur] = 1 + prefix.get(cur,0)
-        return res
-        
+        freq = {0: 1}
+        prefix = 0
+        ans = 0
+
+        for num in nums:
+            prefix += num
+
+            if prefix - k in freq:
+                ans += freq[prefix - k]
+
+            freq[prefix] = freq.get(prefix, 0) + 1
+
+        return ans
